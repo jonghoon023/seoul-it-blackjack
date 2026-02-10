6a808bd1d59fc0c9309a9eeab31ad73073d7ba7f
종훈 <jonghoon023@gmail.com>
2026-02-10 22:42:45 +0900

    코드 개선

---
+++ src/Seoul.It.Blackjack.Backend/Hubs/GameSessionHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Seoul.It.Blackjack.Backend.Hubs;
+
+public class GameSessionHub : Hub
+{
+    public GameSessionHub(IConfiguration configuration)
+    {
+        _dealerKey = configuration["DealerKey"] ?? string.Empty;
+    }
+}
